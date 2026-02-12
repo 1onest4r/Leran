667d348c011f4144c42c9401414eb68fc64ada74
1onest4r <2024054@miu.edu.mn>
2026-02-12 14:25:25 +0800

    last update for clas

--- lib/widgets_layout_demo.dart
+++ lib/widgets_layout_demo.dart
@@ -7,29 +7,39 @@ class WidgetsLayoutDemo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Widgets and layout")),
-      body: Column(
-        children: [
-          Text("Hello"),
-          Text("World"),
-          Padding(padding: const EdgeInsets.all(10.0), child: Text("and MIU")),
-          Icon(Icons.school),
-          IconButton(onPressed: () {}, icon: Icon(Icons.mail_lock)),
-          ElevatedButton(onPressed: () {}, child: Text("Press to terminate")),
-          SizedBox(height: 50),
-          OutlinedButton(
-            onPressed: () {
-              print("pressed these");
-            },
-            child: Text("waaat"),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Text("Hello"),
+              Text("World"),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text("and MIU"),
+              ),
+              Icon(Icons.school),
+              IconButton(onPressed: () {}, icon: Icon(Icons.mail_lock)),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Press to terminate"),
+              ),
+              SizedBox(height: 50),
+              OutlinedButton(
+                onPressed: () {
+                  print("pressed these");
+                },
+                child: Text("waaat"),
+              ),
+              SizedBox(height: 50),
+              OutlinedButton(
+                onPressed: () {
+                  print("pressed those");
+                },
+                child: Text("don't click me"),
+              ),
+            ],
           ),
-          SizedBox(height: 50),
-          OutlinedButton(
-            onPressed: () {
-              print("pressed those");
-            },
-            child: Text("don't click me"),
-          ),
-        ],
+        ),
       ),
     );
   }
