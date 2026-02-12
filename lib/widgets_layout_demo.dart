46d9855b5617f0eecb178ca58717438ce2841ef3
1onest4r <2024054@miu.edu.mn>
2026-02-12 14:14:13 +0800

    learned about how icons and buttons could be used

--- lib/widgets_layout_demo.dart
+++ lib/widgets_layout_demo.dart
@@ -7,7 +7,30 @@ class WidgetsLayoutDemo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Widgets and layout")),
-      body: Column(children: [Text("Hello"), Text("World"), Text("and MIU")]),
+      body: Column(
+        children: [
+          Text("Hello"),
+          Text("World"),
+          Padding(padding: const EdgeInsets.all(10.0), child: Text("and MIU")),
+          Icon(Icons.school),
+          IconButton(onPressed: () {}, icon: Icon(Icons.mail_lock)),
+          ElevatedButton(onPressed: () {}, child: Text("Press to terminate")),
+          SizedBox(height: 50),
+          OutlinedButton(
+            onPressed: () {
+              print("pressed these");
+            },
+            child: Text("waaat"),
+          ),
+          SizedBox(height: 50),
+          OutlinedButton(
+            onPressed: () {
+              print("pressed those");
+            },
+            child: Text("don't click me"),
+          ),
+        ],
+      ),
     );
   }
 }
